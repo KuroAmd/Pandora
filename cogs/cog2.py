b6a74aa111fc9d881005bd3bbf4b801210965e2e
AmdWise <64008263+KuroAmd@users.noreply.github.com>
2021-03-27 03:09:38 +0430

    Update cog2.py
    Added new modules
    Added Avatar & Python cmds

--- cogs/cog2.py
+++ cogs/cog2.py
@@ -2,6 +2,12 @@ import discord
 from discord.ext import commands
 import random
 import datetime
+import asyncio
+import py_compile
+import pydoc
+import pydoc_data
+import re
+
 
 class Moderating(commands.Cog):
 
@@ -19,6 +25,56 @@ class Moderating(commands.Cog):
     @commands.command()
     async def roles(self,ctx, *, member: MemberRoles):
         await ctx.send('{0} got the following roles: '.format(ctx.author) + ', '.join(member))
+
+    @commands.command(aliases=['avatar','av','pfp'])
+    async def Avatar(self, ctx, member: discord.Member=None):
+        if member==None:
+            member=ctx.author
+        em= Embed(title=member.display_name,
+                colour=ctx.author.colour)
+        em.set_image(url=member.avatar_url)
+        await ctx.send(embed=em)
+
+        
+    @commands.command()
+    async def Py(self, ctx, *,args):
+        print(args)
+        if ctx.author.id == 444806806682730496 or 435104102599360522:
+            await ctx.trigger_typing()
+            def python(py_args):
+                py_args = (re.search(r"\(.*\)",py_args).group()[1:-1])
+                return py_args
+            #args = " ".join(args) # this separated every letter!
+            #print(args)
+            if "help" in args:
+                try:
+                    x = python(args)
+                    y = pydoc.getdoc(eval(x))
+                    #print(args)
+                    return await ctx.send(embed = discord.Embed(title=f"Help for {x}",description= f"```Python\n{y}```",timestamp=datetime.datetime.utcnow()))
+                except Exception as e:
+                    return await ctx.send(embed = discord.Embed(tilte="Failure",description=f"```{str(e)}```", timestamp= datetime.datetime.utcnow()))
+
+            try:
+                x = await eval(args)
+            except Exception as e:
+                print(e)
+                #await ctx.send(e)
+                try:
+                    y = eval(args)
+                    #print(f"y = {y}")
+                    if asyncio.iscoroutine(y):
+                        return await ctx.send(embed = discord.Embed(title="Failure.",description=f"{str(y)}",timestamp=datetime.datetime.utcnow()))
+                    else:
+                        return await ctx.send(embed = discord.Embed(title="Success.",description=f"```{str(y)}```",timestamp=datetime.datetime.utcnow()))
+                except Exception as e:
+                    print(e)
+                    return await ctx.send(embed= discord.Embed(title="Failure",description=str(e), timestamp=datetime.datetime.utcnow()))
+            print(f"args = {args}\nx={x}\ty={y}")
+            await ctx.send(embed= discord.Embed(title="Success",description=f"```{str(x)}```",timestamp=datetime.datetime.utcnow()))
+        
+        else:
+            ctx.send("Not allowed to use!")
 
 
     @commands.command(aliases=['prune'])
